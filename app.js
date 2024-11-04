1ff139608cbb8cdf9cac629a2b2182c687440665
Niki Namian <nikinamian@gmail.com>
2024-11-03 20:45:19 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function colorButton(){
+    alert("It's pink!")
+}
+
+function showButton(){
+    alert("It's How I Met Your Mother!")
+}
+
+function bevButton(){
+    alert("It's iced coffee! Specifically iced vanilla lattes.")
+}
